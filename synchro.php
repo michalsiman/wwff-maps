5d44e29075b63da3e05c5cfb80ee8d97e034d642
Michal Siman <michal.siman@gmail.com>
2023-04-04 23:00:49 +0200

    db preparation

--- synchro.php
+++ synchro.php
@@ -1,21 +1,25 @@
 <?php
 // synchro wwff from csv file to mysql db
 // non-working - just as template for re-work
-
+//
 // define db:
-//
-//
-//
-//
+// reference
+// status
+// name
+// dxcc
+// latitude
+// logntitude
+// qsoCount
+// lastAct
 //
 //
 
-$mysqli = new mysqli("localhost", "user", "pass", "csv-to-mysql");
+$mysqli = new mysqli("localhost", "user", "pass", "db_name");
 
 $csvFilePath = "import-template.csv";
 $file = fopen($csvFilePath, "r");
 while (($row = fgetcsv($file)) !== FALSE) {
-    $stmt = $mysqli->prepare("INSERT INTO tbl_users (userName, firstName, lastName) VALUES (?, ?, ?)");
+    $stmt = $mysqli->prepare("INSERT INTO map_points (userName, firstName, lastName) VALUES (?, ?, ?)");
     $stmt->bind_param("sss", $row[1], $row[2], $row[3]);
     $stmt->execute();
 }
